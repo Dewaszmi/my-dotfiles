e27c85fe8fa30bd4a3e14cc2f984ae80ae613ad5
Dewaszmi <maciej.zyczynski@protonmail.com>
2025-08-21 21:05:17 +0200

    tmux and nvim colorscheme updates

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,21 +1,20 @@
 return {
-	"zbirenbaum/copilot.lua",
-	-- cmd = "Copilot",
-	-- event = "InsertEnter",
-	config = function()
-		require("copilot").setup({
-			suggestion = {
-				enabled = true,
-				auto_trigger = false,
-				keymap = {
-					accept = "<C-l>",
-					next = "<M-]>",
-					prev = "<M-[>",
-					dismiss = "<C-]>",
-				},
-			},
-			panel = { enabled = true },
-		})
-	end,
-	lazy = false,
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				suggestion = { enabled = false },
+				panel = { enabled = false },
+			})
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		dependencies = { "zbirenbaum/copilot.lua" },
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+	},
 }
--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -4,12 +4,22 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for file system paths
+		{
+			"L3MON4D3/LuaSnip", -- snippet engine
+			version = "1.2.0",
+			build = "make install_jsregexp",
+		},
+		"saadparwaiz1/cmp_luasnip", -- source for LuaSnip snippets
+		"rafamadriz/friendly-snippets", -- collection of snippets
 		"onsails/lspkind.nvim", -- vs-code like pictograms
 		"kristijanhusak/vim-dadbod-completion", -- sql completion
 	},
 	config = function()
 		local cmp = require("cmp")
+		local luasnip = require("luasnip")
 		local lspkind = require("lspkind")
+
+		require("luasnip.loaders.from_vscode").lazy_load()
 
 		cmp.setup({
 			completion = {
@@ -26,18 +36,25 @@ return {
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({
+				{ name = "copilot" },
+				{ name = "luasnip" }, -- snippets from LuaSnip
 				{ name = "nvim_lua" },
 				{ name = "nvim_lsp" },
 				{ name = "buffer" }, -- text within current buffer
 				{ name = "path" }, -- file system paths
 			}),
-
-			-- configure lspkind for vs-code like pictograms in completion menu
 			formatting = {
-				format = lspkind.cmp_format({
-					maxwidth = 50,
-					ellipsis_char = "...",
-				}),
+				format = function(entry, vim_item)
+					local source_mapping = {
+						copilot = "[ Copilot]",
+						nvim_lsp = "[LSP]",
+						luasnip = "[LuaSnip]",
+						buffer = "[Buf]",
+						path = "[Path]",
+					}
+					vim_item.menu = source_mapping[entry.source.name] or ("[" .. entry.source.name .. "]")
+					return vim_item
+				end,
 			},
 		})
 
